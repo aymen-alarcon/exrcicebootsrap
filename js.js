6cb3748530f42eb01600afccdcad691feedaa201
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-05-15 03:32:54 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+function opensante() {
+  document.getElementById("sante").style.display = "block";
+  document.getElementById("media").style.display = "none";
+  document.getElementById("btp").style.display = "none";
+}
+
+function openmedia() {
+  document.getElementById("sante").style.display = "none";
+  document.getElementById("media").style.display = "block";
+  document.getElementById("btp").style.display = "none";
+}
+
+function openbtp() {
+  document.getElementById("sante").style.display = "none";
+  document.getElementById("media").style.display = "none";
+  document.getElementById("btp").style.display = "block";
+}
