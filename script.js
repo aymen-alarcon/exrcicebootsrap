0c9208d6fd4430094954fb6d031a2c20e3143f18
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-05-17 01:48:55 +0100

    Update and rename js.js to script.js

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ function opensante() {
   document.getElementById("media").style.display = "none";
   document.getElementById("btp").style.display = "none";
 }
-
 function openmedia() {
   document.getElementById("sante").style.display = "none";
   document.getElementById("media").style.display = "block";
   document.getElementById("btp").style.display = "none";
 }
-
 function openbtp() {
   document.getElementById("sante").style.display = "none";
   document.getElementById("media").style.display = "none";
